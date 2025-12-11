5fa05b2abad112ad75bb1896905f2014bd66a940
Joshua Winterflood <joshuatimothy.winterflood@uzh.ch>
2025-12-11 17:42:34 +0100

    remove redundant naming

--- multi_layer_perceptron_with_attention.py
+++ multi_layer_perceptron_with_attention.py
@@ -1,4 +1,3 @@
-from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
@@ -13,10 +12,10 @@ from mlp import Heart_Disease_NN
 
 def log(func):
     def inner(params):
-        logging.info(f"Starting function : {func.__name__} with params : {params[:3]}")
+        logging.info(f"Starting function : {func.__name__} with params : {params[:2]}")
         try:
             func(params)
-            logging.info(f"Ended function {func.__name__} with params : {params[:3]}")
+            logging.info(f"Ended function {func.__name__} with params : {params[:2]}")
         except Exception as e:
             logging.info(f"{func.__name__} failed due to :\n {e}")
     return inner
@@ -24,7 +23,7 @@ def log(func):
 @log
 def process_values(params):
 
-    mlp_dir = os.path.join(os.getcwd(), "results", "MLP_Attention", f"{params[0]}_{params[1]}_{params[2]}")
+    mlp_dir = os.path.join(os.getcwd(), "results", "MLP_Attention", f"{params[0]}_{params[1]}")
     if(os.path.exists(mlp_dir)):
         logging.info(f"Images already computed, skipping recomputation ...")
         return
@@ -33,14 +32,14 @@ def process_values(params):
 
     mlp = Heart_Disease_NN()
 
-    mlp.fit(params[3], params[5])
+    mlp.fit(params[2], params[4])
 
-    y_pred_mlp = mlp.predict(params[4])
-    y_proba_mlp = mlp.predict_proba(params[4])[:, 1]
+    y_pred_mlp = mlp.predict(params[3])
+    y_proba_mlp = mlp.predict_proba(params[3])[:, 1]
 
     #report_all_metrics(params[6], y_pred_mlp, y_proba_mlp, prefix="MLP Classifier")
 
-    cm_mlp = confusion_matrix(params[6], y_pred_mlp)
+    cm_mlp = confusion_matrix(params[5], y_pred_mlp)
     plot_confusion_matrix(
         cm_mlp,
         os.path.join(mlp_dir, "Confusion Matrix"), 
@@ -48,7 +47,7 @@ def process_values(params):
     )
 
     plot_roc_curve(
-        params[6],
+        params[5],
         y_proba_mlp,
         os.path.join(mlp_dir, "ROC Curve"),         
         title="ROC Curve – MLP Classifier"
@@ -56,7 +55,6 @@ def process_values(params):
 
     #torch.save(mlp.state_dict(), os.path.join(mlp_dir, "mlp_state_dict.pt"))
 
-    
 def worker(params):
     set_up_logging()
     process_values(params)
@@ -80,11 +78,10 @@ def main():
     scaler = StandardScaler()
     x_train_scaled, x_test_scaled = scaler.fit_transform(x_train), scaler.transform(x_test)
 
-    hidden_layer_sizes = [[1, 4, 4, 2]]
     learning_rate_inits = [0.0001, 0.001, 0.01, 0.1]
     max_iterations = [200, 400, 600, 800, 1000]
 
-    combinations = itertools.product(hidden_layer_sizes, learning_rate_inits, max_iterations, [x_train_scaled], [x_test_scaled], [y_train], [y_test])
+    combinations = itertools.product(learning_rate_inits, max_iterations, [x_train_scaled], [x_test_scaled], [y_train], [y_test])
 
     for combination in combinations:
         worker(combination)
